3bf3a1189a014ae9fd8cce1a7666042f3f2309a7
Pankaj Garg <86879048+PankajGrg@users.noreply.github.com>
2023-01-23 13:53:24 +0530

    Add files via upload

---
+++ showimg.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = 'volumes_modified/trainMasks_3.npy'
+
+data = np.load(path)
+count = data.shape[0]
+
+# f, plots = plt.subplots(count, 1, figsize=(5, 40))
+
+for i in range(count):
+    image = data[i][0]
+    plt.axis('off')
+    plt.imshow(image)
+    plt.show()
+
